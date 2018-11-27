f2891e8639a33e6da6526b0f4730789f58476549
Chris Russell <cwwrussell@gmail.com>
2018-11-26 18:42:14 -0700

    Theme, Responsive design
    - Swapped the theme colors
    - Fixed the design on the landing page so it is more responsive and lined up on smaller screens

--- src/app/modules/teacher/teacher.module.ts
+++ src/app/modules/teacher/teacher.module.ts
@@ -3,13 +3,14 @@ import {CommonModule} from '@angular/common';
 
 import {TeacherRoutingModule} from './teacher-routing.module';
 import {TeacherComponent} from './pages/teacher/teacher.component';
+import { TeacherHomeComponent } from './pages/teacher-home/teacher-home.component';
 
 @NgModule({
   imports: [
     CommonModule,
     TeacherRoutingModule
   ],
-  declarations: [TeacherComponent]
+  declarations: [TeacherComponent, TeacherHomeComponent]
 })
 export class TeacherModule {
 }
