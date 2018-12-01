78645ae70df28c2761b5dae5e1137df78af6f96c
John Barton <john@barton.link>
2018-12-01 16:52:40 -0700

    Teacher:Data first draft

--- src/app/modules/teacher/pages/teacher-class-data/teacher-class-data.component.ts
+++ src/app/modules/teacher/pages/teacher-class-data/teacher-class-data.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {MatTableDataSource} from '@angular/material';
 import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
@@ -12,6 +13,25 @@ export class TeacherClassDataComponent implements OnInit {
               private router: Router) {
   }
 
+
+  ALPHABET : string[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+  dataSource = new MatTableDataSource<StudentGrade>(GRADE_DATA);
+  displayedColumns : string[] = ['name', ...this.ALPHABET];
+
+  studentId = 'Select Student';
+
+  studentList = [
+    {value: 'Chris', viewValue: '11111111'},
+    {value: 'Heather', viewValue: '222222222'},
+    {value: 'John', viewValue: '33333333333'},
+    {value: 'Cristian', viewValue: '4444444444'},
+  ];
+
+  goToStudentData(student: string) {
+    if(student) this.studentId = student;
+    this.router.navigate([this.studentId === 'Select Student' ? '939393' : this.studentId], {relativeTo: this.activatedRoute});
+  }
+
   ngOnInit() {
   }
 
@@ -20,6 +40,19 @@ export class TeacherClassDataComponent implements OnInit {
   }
 
 }
+
+export interface StudentGrade {
+  name: string,
+  id: string,
+  letters: string[],
+};
+
+const GRADE_DATA: StudentGrade[] = [
+  {name: "Chris", id: "11111111", letters: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','S','T','U','V','W','X','Y','Z']},
+  {name: "Heather", id: "22222222", letters: ['A','B','C','D','E','F','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']},
+  {name: "John", id: "33333333", letters: ['B','C','E','F','G','H','I','J','L','M','O','P','R','S','T','U','W','X','Y','Z']},
+  {name: "Christian", id: "44444444", letters: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','W','X','Y','Z']},
+];
 
 export const route = {
   component: TeacherClassDataComponent,
