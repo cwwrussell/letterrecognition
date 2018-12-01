67e25c0c998d5b2e4d4120b5a8e2e4e5c5752e55
Chris Russell <cwwrussell@gmail.com>
2018-12-01 16:29:27 -0700

    Ripple on nav
    - Added the material ripple effect so the nav is consistent with the other buttons

--- src/app/shared/components/navbar/navbar.module.ts
+++ src/app/shared/components/navbar/navbar.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {NavbarComponent} from './navbar.component';
 import {RouterModule} from '@angular/router';
 import {CommonModule} from '@angular/common';
+import {MatRippleModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
+    MatRippleModule,
   ],
   declarations: [
     NavbarComponent
