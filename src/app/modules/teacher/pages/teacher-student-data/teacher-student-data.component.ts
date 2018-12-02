a3638a306f4e3a446bb478cce3db7f984e4955a4
John Barton <john@barton.link>
2018-12-01 18:33:34 -0700

    Teacher:student data updates

--- src/app/modules/teacher/pages/teacher-student-data/teacher-student-data.component.ts
+++ src/app/modules/teacher/pages/teacher-student-data/teacher-student-data.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {MatTableDataSource} from '@angular/material';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'letters-teacher-student-data',
@@ -7,16 +9,56 @@ import {Component, OnInit} from '@angular/core';
 })
 export class TeacherStudentDataComponent implements OnInit {
 
-  constructor() {
+  constructor(private route: ActivatedRoute) {
   }
 
+  ALPHABET : string[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+  dataSource = new MatTableDataSource<StudentGrade>(GRADE_DATA);
+  displayedColumns : string[] = ['name', ...this.ALPHABET];
+
+ studentId = 'Select Student';
+
+  studentList = [
+    {value: 'Chris', viewValue: '11111111'},
+    {value: 'Heather', viewValue: '22222222'},
+    {value: 'John', viewValue: '33333333'},
+    {value: 'Cristian', viewValue: '44444444'},
+  ];
+
+  private sub: any;
+  
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+        this.studentId = params['studentId'];
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
+
+export interface StudentGrade {
+  name: string,
+  letters: string[],
+};
+
+const letterTestA : string[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('').filter( _ => Math.random() > 0.4 );
+const letterTestB : string[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('').filter( _ => Math.random() > 0.3 );
+const letterTestC : string[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('').filter( _ => Math.random() > 0.2 );
+const letterTestD : string[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('').filter( _ => Math.random() > 0.1 );
+
+const GRADE_DATA: StudentGrade[] = [
+  {name: "Lowercase ABCs Fall", letters: [...letterTestA]},
+  {name: "Uppercase ABCs Fall", letters: [...letterTestB]},
+  {name: "Lowercase ABCs Spring", letters: [...letterTestC]},
+  {name: "Lowercase ABCs Spring", letters: [...letterTestD]},
+];
 
 export const route = {
   component: TeacherStudentDataComponent,
   data: {},
   resolve: {}
 };
+
